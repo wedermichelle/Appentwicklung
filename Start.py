426e51058d6a534149ed28e2922765860176f552
wedermichelle <michelle.weder@bluewin.ch>
2025-02-26 11:17:47 +0100

    Emails

--- Start.py
+++ Start.py
@@ -1,17 +1,13 @@
 import streamlit as st
 import pandas as pd
 
-st.title("Meine erste Streamlit App")
+st.title("Demo App")
 
 # !! WICHTIG: Eure Emails müssen in der App erscheinen!!
 
 # Streamlit über den Text unten direkt in die App - cool!
 """
 Diese App wurde von folgenden Personen entwickelt:
-- Anna Müller (anmu@zhaw.ch)
-- Peter Meier (peme@zhaw.ch)
-
-Diese App ist das leere Gerüst für die App-Entwicklung im Modul Informatik 2 (BMLD/ZHAW)
-
-Autor: Samuel Wehrli (wehs@zhaw.ch)
+- Anna Müller (wedermic@students.zhaw.ch)
+- Peter Meier (pianelis@students.zhaw.ch)
 """
