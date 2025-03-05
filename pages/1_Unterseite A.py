977b2d861b6cbcdda86f5c4ae7b0796ee4cdd80b
wedermichelle <michelle.weder@bluewin.ch>
2025-03-05 11:43:28 +0100

    Buttons hinzugefügt

--- pages/1_Unterseite A.py
+++ pages/1_Unterseite A.py
@@ -1,6 +1,17 @@
 import streamlit as st
 
-st.title("Unterseite A")
+st.title("Blutvolumenrechner")
 
-st.write("Diese Seite ist eine Unterseite der Startseite.")
+st.write("Dieser Rechner berechnet das approximative Blutvolumen anhand der Nadler-Formel. Have fun:)")
+
+st.write("Bitte geben Sie Ihr biologisches Geschlecht an:")
+col1, col2 = st.columns(2)
+
+with col1:
+    if st.button ("Männlich"):
+        st.write("code here")
+
+with col2:
+    if st.button ("Weiblich"):
+        st.write("code here")
 
