c702986b7b6926c83c71d914eab5fe23c9676003
wedermichelle <michelle.weder@bluewin.ch>
2025-03-17 14:57:41 +0100

    Login hinzugefügt

--- Start.py
+++ Start.py
@@ -1,5 +1,18 @@
 import streamlit as st
 import pandas as pd
+from utils.data_manager import DataManager
+from utils.login_manager import LoginManager
+
+data_manager = DataManager(fs_protocol='webdav', fs_root_folder="Blutvolumenrechner")
+login_manager = LoginManager(data_manager)
+login_manager.login_register()
+
+data_manager.load_user_data(
+    session_state_key='data_df', 
+    file_name='data.csv', 
+    initial_value = pd.DataFrame(), 
+    parse_dates = ['timestamp']
+    )
 
 st.title("Demo App")
 
